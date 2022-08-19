800b463bd69e2f787ec135a1398e0bd119024932
moch faisal khoirudin <putrasena21@gmail.com>
2022-08-19 19:55:28 +0700

    add create employee

---
+++ services/userEmployee.services.js
@@ -0,0 +1,22 @@
+const Sequelize = require("sequelize");
+const { user_employee } = require("../models");
+const { Op } = Sequelize;
+
+const createEmployee = async ({ full_name, email, salary }) => {
+  const payload = {
+    full_name,
+    email,
+    created_date: new Date(),
+    updated_date: new Date(),
+    status: "ACTIVE",
+    salary: parseInt(salary, 10),
+  };
+
+  const result = await user_employee.create(payload);
+
+  return result;
+};
+
+module.exports = {
+  createEmployee,
+};
