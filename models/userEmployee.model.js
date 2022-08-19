cd784a5d031351e6096b879f84c7ac737ed46c25
moch faisal khoirudin <putrasena21@gmail.com>
2022-08-19 19:47:09 +0700

    add model user employee

---
+++ models/userEmployee.model.js
@@ -0,0 +1,40 @@
+module.exports = (sequelize, DataTypes) => {
+  const UserEmployee = sequelize.define(
+    "user_employee",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      full_name: {
+        type: DataTypes.STRING,
+      },
+      email: {
+        type: DataTypes.STRING,
+      },
+      created_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updated_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.ENUM,
+        values: ["ACTIVE", "DELETED"],
+      },
+      salary: {
+        type: DataTypes.INTEGER,
+      },
+    },
+    {
+      timestamps: true,
+      createdAt: false, // don't add createdAt attribute
+      updatedAt: false,
+    }
+  );
+  return UserEmployee;
+};
