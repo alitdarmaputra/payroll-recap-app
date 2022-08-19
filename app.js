aea2b3e1b55ec4fb0cdc1cd806a78b3d53044f20
moch faisal khoirudin <putrasena21@gmail.com>
2022-08-19 19:49:58 +0700

    add morgan

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const logger = require("morgan");
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -9,6 +13,6 @@ const indexRouter = require("./routes/index.routes");
 
 app.use("/api/v1", indexRouter);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
